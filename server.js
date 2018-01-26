cef1b9a8bf9ad0032bc38d63193fd80acf3286fe
jagadesana <jagadesana@hpe.com>
2018-01-26 12:08:26 +0530

    second upload

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('build'));
 
+app.get("/", function (req, res) {
+    res.sendFile('build/index.html', {root: __dirname})
+});
+
 server.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
