4491ce32acae067336f2376048a0a2b4c236ac23
jagadesana <jagadesana@hpe.com>
2018-01-26 12:29:43 +0530

    second upload

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const path = require('path');
-var cors = require('cors');
-var bodyParser = require("body-parser");
 
 var app = express();
 var server = require('http').Server(app);
-
-app.use(bodyParser.json());
-
-app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
