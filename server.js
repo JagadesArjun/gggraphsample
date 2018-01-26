d63fabb944485e8a39b9df10362ab78bce7c1854
jagadesana <jagadesana@hpe.com>
2018-01-26 12:05:17 +0530

    second upload

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
 
-express()
-    .use(express.static(path.join(__dirname, 'build')))
-    .set('view engine', 'html')
-    .get('/', (req, res) => res.render('build/index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var app = express();
+var server = require('http').Server(app);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('build'));
+
+server.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
