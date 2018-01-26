dcfa5ba015ab7daaaa6148ac611a1e28c911a8eb
jagadesana <jagadesana@hpe.com>
2018-01-26 11:54:16 +0530

    second upload

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 5000;
 
 express()
     .use(express.static(path.join(__dirname, 'build')))
+    .get('/', (req, res) => res.render('build/index'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
